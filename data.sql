ab551c6f09c7e6cbaef374c6b9671598a5c7657c
Cynthia <ivyc8192@gmail.com>
2019-03-15 10:57:14 -0700

    create user table

--- data.sql
+++ data.sql
@@ -16,6 +16,16 @@ CREATE TABLE jobs (
     CONSTRAINT equity_val_check CHECK ((equity < (1)::double precision))
 );
 
+CREATE TABLE users (
+    username text PRIMARY KEY,
+    password text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email text NOT NULL UNIQUE,
+    photo_url text,
+    is_admin boolean DEFAULT false NOT NULL
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 200, null, null),
          ('pixar', 'Pixar Animation Studios', 3000, 'the best', null),
@@ -24,3 +34,7 @@ INSERT INTO companies
 INSERT INTO jobs (title, salary, equity, company_handle)
   VALUES ( 'developer', 200000, .1, 'pixar'),
          ( 'animator', 100000, 0.05, 'pixar');
+
+INSERT INTO users (username, password, first_name, last_name, email, photo_url)
+  VALUES ( 'coconut', 'popcorn', 'Whiskey', 'Land', 'whiskey@dog.com', NULL),
+         ( 'spike', 'popcorn', 'Spkie', 'Land', 'spike@dog.com', NULL);
