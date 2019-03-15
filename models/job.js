23299bfcbb2d9dada2fa1f9391d0680b9eaf7b1e
meowcodes <meowcodes@Sams-iPhone.localdomain>
2019-03-15 09:17:30 -0700

    edit error

--- models/job.js
+++ models/job.js
@@ -93,8 +93,8 @@ class Job {
 
     static async getOne(id) {
 
-        if(isNaN(id)){
-            throw {message: "Id must be a number", status: 400}
+        if(!Number.isInteger(id)){
+            throw {message: "Id must be an integer", status: 400}
         }
         
         const result = await db.query(`
@@ -135,8 +135,8 @@ class Job {
      */
 
     static async delete(id) {
-        if(isNaN(id)){
-            throw {message: "Id must be a number", status: 400}
+        if(!Number.isInteger(id)){
+            throw {message: "Id must be an integer", status: 400}
         }
         
         const result = await db.query(`
